5a29dc7bb9e55aac104a324e04b7eb3b82cde2e3
Tim nyarko <einsteinowusu55@gmail.com>
2024-11-17 20:59:16 +0000

    first commit

--- Sates/__init__.py
+++ Sates/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_bcrypt import  Bcrypt
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy 
-import os
-from decouple import config
 
 
 app = Flask(__name__)
@@ -11,7 +9,7 @@ app = Flask(__name__)
 # Configure the database URI
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///satellite.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-SECRET_KEY = config('SECRET_KEY')
+app.config['SECRET_KEY']= '9200cd4bb68d616a50925a5c'
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login_page"

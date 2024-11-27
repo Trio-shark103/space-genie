c350bdd9b713c0db24b758649bbc5e01f9ee0c89
Tim nyarko <einsteinowusu55@gmail.com>
2024-11-27 20:31:01 +0000

    Added a download link

--- Sates/__init__.py
+++ Sates/__init__.py
@@ -1,6 +1,4 @@
 from flask import Flask
-from flask_bcrypt import  Bcrypt
-from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy 
 
 
@@ -9,11 +7,8 @@ app = Flask(__name__)
 # Configure the database URI
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///satellite.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config['SECRET_KEY']= '9200cd4bb68d616a50925a5c'
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = "login_page"
-login_manager.login_message_category = "info"
+
+
 # Initialize the database
 db = SQLAlchemy(app)
 
--- Sates/routes.py
+++ Sates/routes.py
@@ -1,10 +1,14 @@
 from Sates import app, db
-from flask import render_template,request, redirect, url_for, flash
-from Sates.models import Satellite, User
-from Sates.forms import RegisterForm, LoginForm
-from flask_login import login_user, logout_user, login_required
+from flask import render_template,request, redirect, url_for, Response
+from Sates.models import Satellite
 from flask_sqlalchemy import SQLAlchemy
 import os
+from io import BytesIO
+from reportlab.lib.pagesizes import LEDGER
+from reportlab.pdfgen import canvas
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from reportlab.lib import colors
+
 
 
 @app.route('/')
@@ -39,49 +43,64 @@ def sate_update():
         
     return render_template("Updates.html", satellites=satellites)
 
-# route to register the new users
-@app.route('/register', methods=['GET','POST'])
-def register_page():
-    form= RegisterForm()
-    #Adding Validation to the submit button
-    if form.validate_on_submit():
-        user_to_create = User(username=form.username.data,
-                            email_address=form.email_address.data,
-                            password=form.password1.data)
-        db.session.add(user_to_create)
-        db.session.commit()
-        login_user(user_to_create)
-        flash(f'Account created successfully! You are now logged in as {user_to_create.username}', category="success")
-        
-        return redirect(url_for('sate_update'))
-    if form.errors !={}: #if any error occur during validations
-        for err_msg in form.errors.values():
-            flash(f'There was an error in creating a user: {err_msg}', category='danger')
+@app.route("/download", methods=["GET"])
+def download_pdf():
+    # Query all satellites
+    satellites = Satellite.query.all()
+    
+    table_data = [["id", "Continent","Satellite Name", "Satellite Type","Manufacturer", "Cost (in million USD)", ]]  # Table header
+    for satellite in satellites:
+        table_data.append([
+            satellite.id,
+            satellite.continent,
+            satellite.name,
+            satellite.satellite_type,
+            satellite.manufacturer,
+            f"${satellite.cost:,.2f}", #.2f is to add 2 decimal places to the integer
             
-    return render_template('register.html', form=form)
+        ])
+
+    # Create a PDF in-memory
+    buffer = BytesIO()
+    pdf = SimpleDocTemplate(buffer, pagesize=LEDGER)
+    elements = []
+
+    # Create a Table with styles
+    column_widths = [20, 120, 250, 250, 250, 100] #setting a column widths for the table
+    table = Table(table_data, colWidths=column_widths)
+    table_style = TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),  # add Header for background
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),  # add Header for text color
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),  # Center align all cells
+        ('FONTNAME', (0, 0), (-1, -1), 'Helvetica-Bold'),  # Header font bold
+        ('FONTSIZE', (0, 0), (-1, -1), 8),  # Reduce font size
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),  # Header bottom padding
+        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),  # Alternate row background
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),  # Add grid lines
+    ])
+    table.setStyle(table_style)
+
+    # Add the table to the elements list
+    elements.append(table)
+
+    # Build the PDF
+    pdf = SimpleDocTemplate(
+        buffer,
+        pagesize=LEDGER,
+        leftMargin=20,
+        topMargin=20,
+        bottomMargin=20
+    )
+    pdf.build(elements)
+
+    # Prepare the response
+    buffer.seek(0)
+    response = Response(buffer.getvalue(), mimetype="application/pdf")
+    response.headers["Content-Disposition"] = "attachment; filename=satellites.pdf"
+
+    return response
 
 
-#route to enable users to login on the page .
-@app.route('/login', methods=['GET','POST'])
-def login_page():
-    form=LoginForm()
-    #Adding validation to the login button
-    if form.validate_on_submit():
-        attempted_user =User.query.filter_by(username=form.username.data).first()
-        if attempted_user and attempted_user.check_password_correction(
-            attempted_password=form.password.data
-        ):
-            login_user(attempted_user)
-            flash(f'Success! You are logged in as: {attempted_user.username}', category='Success')
-            return redirect(url_for('sate_update'))
-        else:
-            flash('Username and password are not match! Please try again',category='danger')
-            
-    return render_template('login.html', form=form)
+    
 
-#route to enable users to logout off the page
-@app.route('/logout')
-def logout_page():
-    logout_user()
-    flash('You have been logged out!', category='info')
-    return redirect(url_for('home_page'))
+
